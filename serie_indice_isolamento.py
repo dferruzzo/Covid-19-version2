5a18f5f30441e09468910e0c9b6331df1c6179c4
Diego Ferruzzo <dferruzzo@gmail.com>
2023-03-07 15:55:56 +0000

    gráfico idx isolamento

---
+++ serie_indice_isolamento.py
@@ -0,0 +1,30 @@
+"""
+Script carregar os dados da série completa do índice de isolamento
+------------------------------------------------------------------
+Não tem argumentos de entrada.
+Produz uma figura de saída
+"""
+import pandas as pd
+from parameters import *
+from numpy import arange
+import time
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+def serie_indice_isolamento():
+    # carregando data do índice de isolamento
+    print('Carregando dados do índice de isolamento...')
+    df = pd.read_csv("data/SaoPaulo_isolamento.csv")    
+    df.drop(labels='Unnamed: 0', axis=1, inplace=True)
+    df.rename(columns={'Isol': 'Dados'}, inplace=True)
+    df.set_index('Data', inplace=True)
+    #
+    plt.figure(figsize=(20,5), dpi=80)
+    plt.plot(df.index, df['Dados'])
+    plt.plot(df.index, df['Dados'],'o', color='tab:blue')
+    plt.ylabel('Isolation Index')
+    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=7))
+    plt.xticks(rotation=90)
+    plt.show()
+    #
+    return
